c2f15d1360b8760ee8e125fdd54cf69ecda711f0
Paramveer <paramsingh1205@outlook.com>
2023-06-11 12:44:07 +0530

    added input and button functionality

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 let sizeOption = document.querySelector("input");
 sizeOption.addEventListener("change", () => {
   let selectedSize = sizeOption.value;
-  createBoard(selectedSize);
+  if (selectedSize <= 100) {
+    createBoard(selectedSize);
+  } else {
+    alert("Max value is 100");
+  }
 });
 
 function createBoard(size) {
@@ -16,7 +20,22 @@ function createBoard(size) {
   let numsDivs = size * size;
   for (let i = 0; i < numsDivs; i++) {
     let div = document.createElement("div");
-    div.style.background = "yellow";
+    let blackbtn = document.querySelector(".blackbtn");
+    let randombtn = document.querySelector(".randombtn");
+    let resetbtn = document.querySelector(".resetbtn");
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
     canvas.insertAdjacentElement("beforeend", div);
+
+    blackbtn.addEventListener("click", () => {
+      div.style.backgroundColor = "black";
+    });
+    randombtn.addEventListener("click", () => {
+      div.style.backgroundColor = "black";
+    });
+    resetbtn.addEventListener("click", () => {
+      div.style.backgroundColor = "white";
+    });
   }
 }
