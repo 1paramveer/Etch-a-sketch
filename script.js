4d073f2713420b46a583c38b28a40d60355b08b1
Paramveer <paramsingh1205@outlook.com>
2023-06-11 12:23:59 +0530

    added input for divs size

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   createBoard(16);
 });
 
+let sizeOption = document.querySelector("input");
+sizeOption.addEventListener("change", () => {
+  let selectedSize = sizeOption.value;
+  createBoard(selectedSize);
+});
+
 function createBoard(size) {
   let canvas = document.querySelector(".canvas");
   canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
